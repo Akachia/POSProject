ccce713257647ef562268933acef9b4cca081e28
Akachia <carryon72@naver.com>
2018-01-17 18:23:06 +0900

    2018년 1월 18일자 통합 및 코딩 작업분입니다.

---
+++ POSproject/Form_order.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace POSproject_KSM
+{
+    partial class order_From
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_Exit = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lbl_TtlPrice = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lbl_TtlsellPrice = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1});
+            this.dataGridView1.Location = new System.Drawing.Point(0, 130);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToDisplayedHeaders;
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(800, 385);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "제품 선택";
+            this.Column1.Name = "Column1";
+            this.Column1.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Column1.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(800, 63);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(480, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label3";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(231, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(14, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // btn_Exit
+            // 
+            this.btn_Exit.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btn_Exit.Location = new System.Drawing.Point(721, 521);
+            this.btn_Exit.Name = "btn_Exit";
+            this.btn_Exit.Size = new System.Drawing.Size(75, 75);
+            this.btn_Exit.TabIndex = 10;
+            this.btn_Exit.Text = "나가기";
+            this.btn_Exit.UseVisualStyleBackColor = true;
+            this.btn_Exit.Click += new System.EventHandler(this.btn_Exit_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(643, 521);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 75);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "보내기";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // lbl_TtlPrice
+            // 
+            this.lbl_TtlPrice.AutoSize = true;
+            this.lbl_TtlPrice.Font = new System.Drawing.Font("굴림", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lbl_TtlPrice.Location = new System.Drawing.Point(524, 82);
+            this.lbl_TtlPrice.Name = "lbl_TtlPrice";
+            this.lbl_TtlPrice.Size = new System.Drawing.Size(26, 24);
+            this.lbl_TtlPrice.TabIndex = 12;
+            this.lbl_TtlPrice.Text = "  ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("굴림", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label5.Location = new System.Drawing.Point(348, 82);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(170, 24);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "총 발주 가격 : ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("굴림", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label4.Location = new System.Drawing.Point(14, 82);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(170, 24);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "총 발주 가격 : ";
+            // 
+            // lbl_TtlsellPrice
+            // 
+            this.lbl_TtlsellPrice.AutoSize = true;
+            this.lbl_TtlsellPrice.Font = new System.Drawing.Font("굴림", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lbl_TtlsellPrice.Location = new System.Drawing.Point(190, 82);
+            this.lbl_TtlsellPrice.Name = "lbl_TtlsellPrice";
+            this.lbl_TtlsellPrice.Size = new System.Drawing.Size(26, 24);
+            this.lbl_TtlsellPrice.TabIndex = 14;
+            this.lbl_TtlsellPrice.Text = "  ";
+            // 
+            // order_From
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 600);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lbl_TtlsellPrice);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lbl_TtlPrice);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn_Exit);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "order_From";
+            this.Text = "Order";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.order_From_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_Exit;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lbl_TtlPrice;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Column1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lbl_TtlsellPrice;
+    }
+}
---
+++ POSproject/Prcd.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POSproject
+{
+    class Prcd
+    {
+        public DateTime checkin;
+        static public bool LogIn(string id, string pwd)
+        {
+            if(CheckID(id))
+            {
+                if(CheckPwd(id,pwd))
+                {
+                    return true;
+                }
+                else
+                {
+                    System.Windows.Forms.MessageBox.Show("비밀번호가 맞지 않습니다!");
+                    return false;
+                }
+            }
+            else
+            {
+                System.Windows.Forms.MessageBox.Show("ID를 확인해주세요!");
+                return false;
+            }
+            
+        }
+
+
+        static private bool CheckID(string id)
+        {
+            
+           
+            using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["PosSystem"].ConnectionString))
+            {
+                //con.Open();
+                using (var cmd = new SqlCommand("CheckID", con))
+                {
+                    con.Open();
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@userid", id);
+
+                    var sdr = cmd.ExecuteReader();
+
+                    if (sdr.HasRows)
+                    {
+
+
+                        sdr.Close();
+                        return true;
+                    }
+                    else
+                    {
+                        sdr.Close();
+                        return false;
+                    }
+                }
+            }
+        }
+
+        static public string CheckAdmin(string id)
+        {
+
+
+            using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["PosSystem"].ConnectionString))
+            {
+                //con.Open();
+                string admin=null;
+                using (var cmd = new SqlCommand("CheckAdmin", con))
+                {
+                    con.Open();
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@userID", id);
+
+                    var sdr = cmd.ExecuteReader();
+                    while(sdr.Read())
+                    {
+                        if (!(sdr["Authority"].ToString() == null))
+                        {
+                            admin= sdr["Authority"].ToString();
+                            //System.Windows.Forms.MessageBox.Show("Test");
+                        }
+                        
+                    }
+                    
+                }
+                return admin;
+            }
+        }
+
+
+        static private bool CheckPwd(string id, string pwd)
+        {
+            
+            using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["PosSystem"].ConnectionString))
+            {
+                //con.Open();
+                using (var cmd = new SqlCommand("CheckPwd", con))
+                {
+                    con.Open();
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@userid", id);
+                    cmd.Parameters.AddWithValue("@userPwd", pwd);
+                    
+
+                    var sdr = cmd.ExecuteReader();
+
+                    if (sdr.HasRows)
+                    {
+
+
+                        sdr.Close();
+                        return true;
+                    }
+                    else
+                    {
+                        sdr.Close();
+                        return false;
+                    }
+                }
+            }
+        }
+    }
+}
